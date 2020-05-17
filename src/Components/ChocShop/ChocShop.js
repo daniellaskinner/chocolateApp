de502691ea47ad62b6ed3362ba42c0072c9308b1
Nelly <daniellaskinner120@gmail.com>
2020-05-17 13:22:32 +0100

    created show product function in choc shop component and passed to child component product.js through props named show

--- src/Components/ChocShop/ChocShop.js
+++ src/Components/ChocShop/ChocShop.js
@@ -4,13 +4,18 @@ import Total from './Total/Total';
 import './ChocShop.css';
 
 class ChocShop extends React.Component{
+
+  showProduct = (name) => {
+    alert("You selected " + name)
+  };
+
   render() {
     return(
       <div className="chocProductContainer">
         <h2>This is the chocolate shop!</h2>
-        <Product name="Crunchie" price="60"/>
-        <Product name="MarsBar" price="65"/>
-        <Product name="Kinder Bueno" price="90"/>
+        <Product name="Crunchie" price="60" handleShow={this.showProduct}/>
+        <Product name="MarsBar" price="65" handleShow={this.showProduct}/>
+        <Product name="Kinder Bueno" price="90" handleShow={this.showProduct}/>
         <Total/>
       </div>
     );
--- src/Components/ChocShop/Product/Product.js
+++ src/Components/ChocShop/Product/Product.js
@@ -11,11 +11,16 @@ class Product extends React.Component {
     this.setState({qty: this.state.qty + 1});
   };
 
+  show = () => {
+    this.props.handleShow(this.props.name)
+  };
+
   render() {
     return (
       <div className="chocProduct">
         <p>{this.props.name} - {this.props.price}p</p>
         <button onClick = {this.handleClick}>Buy</button>
+        <button onClick = {this.show}>Show</button>
         <h3>Quantity: {this.state.qty} bar(s)</h3>
       </div>
     );
